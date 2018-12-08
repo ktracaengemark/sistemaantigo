a9435bf57d7ec1be9dfa142de5634eb6721a80ab
ktracaengemark-Itau <marciorodeng@gmail.com>
2018-12-08 15:50:34 -0200

    limpeza

--- comercio/retornaCliente.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $pdo = new PDO("mysql:host=localhost; dbname=app; charset=utf8;", "root", "", $opcoes);
-    $dados = $pdo->prepare("SELECT NomeProfissional FROM App_Profissional");
-    $dados->execute();
-    echo json_encode($dados->fetchAll(PDO::FETCH_ASSOC));
-    $json = '[';
-    $first = true;
-?>
