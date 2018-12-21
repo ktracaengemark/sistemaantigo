59b30282b0de7bdf8d9bb56954f4119d88805ac2
ktracaengemark-Itau <marciorodeng@gmail.com>
2018-12-21 18:52:34 -0200

    Logo da Empresa no Acesso dos usuários

--- application/views/acesso/tela_acesso.php
+++ application/views/acesso/tela_acesso.php
@@ -1,33 +1,24 @@
-<br>
 <?php if (isset($msg)) echo $msg; ?>
 <?php if ( !isset($evento) && isset($_SESSION['log'])) { ?>
-
 <div class="container-fluid">
 	<div class="row">
-
-		<div class="col-md-3"></div>
-		<div class="col-md-6 text-center t">
-		
+		<div class="col-sm-offset-3 col-md-6 text-center t">
 			<div class="panel panel-primary">
 				<div class="panel-heading"></div>
 				<div class="panel-body">
 					<div class="col-md-12 text-center t">
-						<h3><?php echo '<small>Bem Vindo<br> </small><strong>"' . $_SESSION['log']['Nome'] . '"</strong>'  ?></h3>
+						<h4><?php echo '<small>Bem Vindo<br> </small><strong>"' . $_SESSION['log']['Nome'] . '"</strong>'  ?></h4>
+						<h4><?php echo '<small>a(o)<br></small><strong> ' . $_SESSION['log']['NomeEmpresa'] . '</strong>.'  ?></h4>
 					</div>
-
-					<div class="col-md-12 text-center t">
-						<h3><?php echo '<small>Usuario da(o)<br></small><strong> ' . $_SESSION['log']['NomeEmpresa'] . '</strong>.'  ?></h3>
+					<div class="col-sm-offset-4 col-lg-4 " align="center"> 
+						<img alt="User Pic" src="<?php echo base_url() . 'arquivos/imagens/empresas/profile-' . $_SESSION['log']['idSis_Empresa'] . '.png'; ?>" class="img-circle img-responsive">
 					</div>
-
 					<div class="col-md-12 text-center t">
-						<h3><?php echo '<small>Acesse o </small><strong> Menu </strong><small> acima <br>e tenha um bom trabalho! </small>'  ?></h3>
+						<h4><?php echo '<small>Acesse o </small><strong> Menu </strong><small> acima <br>e tenha um bom trabalho! </small>'  ?></h4>
 					</div>
 				</div>	
 			</div>		
 		</div>
-		<div class="col-md-3"></div>
-
 	</div>
 </div>	
-
-	<?php } ?>
+<?php } ?>
