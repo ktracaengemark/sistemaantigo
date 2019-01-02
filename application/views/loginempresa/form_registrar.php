d4fbf5a3bbf7384932e262ae7b442e6686896f9e
ktracaengemark-Itau <marciorodeng@gmail.com>
2019-01-02 11:24:46 -0200

    Correção do texto de chamada da Política de Privacidade

--- application/views/loginempresa/form_registrar.php
+++ application/views/loginempresa/form_registrar.php
@@ -12,11 +12,11 @@
 					<div class="modal-header bg-danger">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 						<h4 class="modal-title">Ao "Confirmar Registro",<br>
-												Você concorda com a Política de Privacidade!</h4>
+												Você concorda com a nossa Política de Privacidade!</h4>
 					</div>
 					
 					<div class="modal-body">
-						<p>Plítica de Privacidade.....</p>
+						<p>Política de Privacidade.....</p>
 					</div>
 					
 					<div class="modal-footer">
--- application/views/loginempresa/form_registrar2.php
+++ application/views/loginempresa/form_registrar2.php
@@ -12,7 +12,7 @@
 					<div class="modal-header bg-danger">
 						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
 						<h4 class="modal-title">Ao "Confirmar Registro",<br>
-												Você concorda com Política de Privacidade!</h4>
+												Você concorda com a nossa Política de Privacidade!</h4>
 					</div>
 					
 					<div class="modal-body">
