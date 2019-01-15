7db395b6ca8c9e941aa076b29802677afd8ced3c
ktracaengemark-Itau <marciorodeng@gmail.com>
2019-01-15 19:34:30 -0200

    Ajuste nas Telas dos Formulários de Registro

--- application/views/login/form_registrar.php
+++ application/views/login/form_registrar.php
@@ -1,6 +1,6 @@
 <?php if (isset($msg)) echo $msg; ?>
 
-<div class="container col-sm-offset-3 col-md-3 ">
+<div class="container col-sm-offset-2 col-md-4 ">
 	<?php echo form_open('login/registrar', 'role="form"'); ?>
 	
 	<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
@@ -44,120 +44,128 @@
 	</div>
 
 	<h2 class="form-signin-heading text-center">enkontraki</h2>		 
-	<center>
-		<figure>
-			<div class="boxVideo">
-				<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
-				<iframe width="255" height="255" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
-			</div>
-		</figure>
-	</center>
-	<script type="text/javascript">
-		$('#ca-container').contentcarousel();
-	</script>	
-	<div class="container col-sm-offset-2 col-md-8 ">
-		<label class="sr-only">Empresa</label>
-		<select data-placeholder="Selecione uma opção..." class="form-control" id="idSis_Empresa" name="idSis_Empresa">			
+	<div class="container col-md-5 ">
+		<div class="row">	
+			<center>
+				<figure>
+					<div class="boxVideo">
+						<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
+						<iframe width="230" height="230" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</figure>
+			</center>
+			<script type="text/javascript">
+				$('#ca-container').contentcarousel();
+			</script>
 			
-			<?php
-			foreach ($select['idSis_Empresa'] as $key => $row) {
-				(!$query['idSis_Empresa']) ? $query['idSis_Empresa'] = '5' : FALSE;
-				if ($query['idSis_Empresa'] == $key) {
-					echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
-				} else {
-					echo '<option value="' . $key . '">' . $row . '</option>';
+			<label class="sr-only">Empresa</label>
+			<select data-placeholder="Selecione uma opção..." class="form-control" id="idSis_Empresa" name="idSis_Empresa">			
+				
+				<?php
+				foreach ($select['idSis_Empresa'] as $key => $row) {
+					(!$query['idSis_Empresa']) ? $query['idSis_Empresa'] = '5' : FALSE;
+					if ($query['idSis_Empresa'] == $key) {
+						echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
+					} else {
+						echo '<option value="' . $key . '">' . $row . '</option>';
+					}
 				}
-			}
-			?>   
-		</select>
-		<?php echo form_error('idSis_Empresa'); ?>
-		<br>
-		
-		<label for="Nome">Nome do Usuário:</label>
-		<input type="text" class="form-control" id="Nome" maxlength="255"
-			   name="Nome" value="<?php echo $query['Nome']; ?>">
-		<?php echo form_error('Nome'); ?>
-		<br>
-		
-		<label for="Celular">Celular:</label>
-		<input type="text" class="form-control Celular Celular" id="Celular" maxlength="11"
-			   name="Celular" placeholder="(XX)999999999" value="<?php echo $query['Celular']; ?>">
-		<?php echo form_error('Celular'); ?>
-		<br>
+				?>   
+			</select>
+			<?php echo form_error('idSis_Empresa'); ?>
+			<br>
+			
+			<label for="Nome">Nome do Usuário:</label>
+			<input type="text" class="form-control" id="Nome" maxlength="255"
+				   name="Nome" value="<?php echo $query['Nome']; ?>">
+			<?php echo form_error('Nome'); ?>
+			<br>
+			
+			<label for="Celular">Celular:</label>
+			<input type="text" class="form-control Celular Celular" id="Celular" maxlength="11"
+				   name="Celular" placeholder="(XX)999999999" value="<?php echo $query['Celular']; ?>">
+			<?php echo form_error('Celular'); ?>
+			<br>
 
-		<label for="DataNascimento">Data de Nascimento:</label>
-		<input type="text" class="form-control Date" id="inputDate0" maxlength="10"
-			   name="DataNascimento" placeholder="DD/MM/AAAA" value="<?php echo $query['DataNascimento']; ?>">
-		<?php echo form_error('DataNascimento'); ?>
-		<br>
+		<!--
+			<label class="text-">E-mail:</label>
+			<input type="text" class="form-control" id="Email" maxlength="100"
+				   name="Email" value="<?php #echo $query['Email']; ?>">
+			<?php #echo form_error('Email'); ?>
+			<br>
 
-		<label for="Sexo">Sexo:</label>
-		<select data-placeholder="Selecione um TROCA..." class="form-control" id="Sexo" name="Sexo">
-			<option value=""></option>
-			<?php
-			foreach ($select['Sexo'] as $key => $row) {
-				if ($query['Sexo'] == $key) {
-					echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
-				} else {
-					echo '<option value="' . $key . '">' . $row . '</option>';
+			<label class="text-">Confirmar E-mail:</label>
+			<input type="text" class="form-control" id="ConfirmarEmail" maxlength="100"
+				   name="ConfirmarEmail" value="<?php #echo $query['ConfirmarEmail']; ?>">
+			<?php #echo form_error('ConfirmarEmail'); ?>
+			<br>
+			
+			<label for="Usuario">Usuário:</label>
+			<input type="text" class="form-control" id="Usuario" maxlength="45"
+				   name="Usuario" value="<?php #echo $query['Usuario']; ?>">
+			<?php #echo form_error('Usuario'); ?>
+			<br>
+		-->
+		</div>
+	</div>
+	<div class="col-md-1 "></div>
+	<div class="container col-md-5 ">	
+		<div class="row">			
+			<label for="DataNascimento">Data de Nascimento:</label>
+			<input type="text" class="form-control Date" id="inputDate0" maxlength="10"
+				   name="DataNascimento" placeholder="DD/MM/AAAA" value="<?php echo $query['DataNascimento']; ?>">
+			<?php echo form_error('DataNascimento'); ?>
+			<br>
+
+			<label for="Sexo">Sexo:</label>
+			<select data-placeholder="Selecione um TROCA..." class="form-control" id="Sexo" name="Sexo">
+				<option value=""></option>
+				<?php
+				foreach ($select['Sexo'] as $key => $row) {
+					if ($query['Sexo'] == $key) {
+						echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
+					} else {
+						echo '<option value="' . $key . '">' . $row . '</option>';
+					}
 				}
-			}
-			?>
-		</select>
-		<?php echo form_error('Sexo'); ?>
-		<br>
-	<!--
-		<label class="text-">E-mail:</label>
-		<input type="text" class="form-control" id="Email" maxlength="100"
-			   name="Email" value="<?php #echo $query['Email']; ?>">
-		<?php #echo form_error('Email'); ?>
-		<br>
+				?>
+			</select>
+			<?php echo form_error('Sexo'); ?>
+			<br>
+			
+			<label for="CpfUsuario">CPF do Usuário:</label>
+			<input type="text" class="form-control " id="CpfUsuario" maxlength="11"
+				   name="CpfUsuario" placeholder="99999999999" value="<?php echo $query['CpfUsuario']; ?>">
+			<?php echo form_error('CpfUsuario'); ?>
+			<br>
+			
+			<label for="Senha">Senha:</label>
+			<input type="password" class="form-control" id="Senha" maxlength="45"
+				   name="Senha" value="<?php echo $query['Senha']; ?>">
+			<?php echo form_error('Senha'); ?>
+			<br>
 
-		<label class="text-">Confirmar E-mail:</label>
-		<input type="text" class="form-control" id="ConfirmarEmail" maxlength="100"
-			   name="ConfirmarEmail" value="<?php #echo $query['ConfirmarEmail']; ?>">
-		<?php #echo form_error('ConfirmarEmail'); ?>
-		<br>
-		
-		<label for="Usuario">Usuário:</label>
-		<input type="text" class="form-control" id="Usuario" maxlength="45"
-			   name="Usuario" value="<?php #echo $query['Usuario']; ?>">
-		<?php #echo form_error('Usuario'); ?>
-		<br>
-	-->
-		<label for="CpfUsuario">CPF do Usuário:</label>
-		<input type="text" class="form-control " id="CpfUsuario" maxlength="11"
-			   name="CpfUsuario" placeholder="99999999999" value="<?php echo $query['CpfUsuario']; ?>">
-		<?php echo form_error('CpfUsuario'); ?>
-		<br>
-		
-		<label for="Senha">Senha:</label>
-		<input type="password" class="form-control" id="Senha" maxlength="45"
-			   name="Senha" value="<?php echo $query['Senha']; ?>">
-		<?php echo form_error('Senha'); ?>
-		<br>
+			<label for="Senha">Confirmar Senha:</label>
+			<input type="password" class="form-control" id="Confirma" maxlength="45"
+				   name="Confirma" value="<?php echo $query['Confirma']; ?>">
+			<?php echo form_error('Confirma'); ?>
+			<br>
+		<!--
+			<button class="btn btn-lg btn-success btn-block" type="submit">REGISTRAR</button>
+			<br>
+		-->
+			<button  class="btn btn-lg btn-success btn-block" type="button" data-toggle="modal" data-loading-text="Aguarde..." data-target=".bs-excluir-modal-sm">
+				<span class="glyphicon glyphicon-floppy-saved"></span> Registrar
+			</button>
 
-		<label for="Senha">Confirmar Senha:</label>
-		<input type="password" class="form-control" id="Confirma" maxlength="45"
-			   name="Confirma" value="<?php echo $query['Confirma']; ?>">
-		<?php echo form_error('Confirma'); ?>
-		<br>
-	<!--
-		<button class="btn btn-lg btn-success btn-block" type="submit">REGISTRAR</button>
-		<br>
-	-->
-		<button  class="btn btn-lg btn-success btn-block" type="button" data-toggle="modal" data-loading-text="Aguarde..." data-target=".bs-excluir-modal-sm">
-			<span class="glyphicon glyphicon-floppy-saved"></span> Registrar
-		</button>
+			<br>
+			
+			<a class="btn btn-lg btn-danger btn-block" href="<?php echo base_url(); ?>login/index" role="button">
+				<span class="glyphicon glyphicon-floppy-remove"></span>	Cancelar
+			</a>
+			<!--<a class="btn btn btn-warning btn-block" href="<?php #echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>-->
 
-		<br>
-		
-		<a class="btn btn-lg btn-danger btn-block" href="<?php echo base_url(); ?>login/index" role="button">
-			<span class="glyphicon glyphicon-floppy-remove"></span>	Cancelar
-		</a>
-		<!--<a class="btn btn btn-warning btn-block" href="<?php #echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>-->
-		<br><br>
-
+		</div>
 	</div>
 	
 </div>
--- application/views/loginempresa/form_registrar2.php
+++ application/views/loginempresa/form_registrar2.php
@@ -1,6 +1,6 @@
 <?php if (isset($msg)) echo $msg; ?>	
 	
-<div class="container col-sm-offset-3 col-md-3 ">
+<div class="container col-sm-offset-2 col-md-4 ">
 	<?php echo form_open('loginempresa/registrar2', 'role="form"'); ?>
 	
 	<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
@@ -43,138 +43,145 @@
 		</div>
 	</div>
 	<h2 class="form-signin-heading text-center">enkontraki</h2>		 
-	<center>
-		<figure>
-			<div class="boxVideo">
-				<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
-				<iframe width="255" height="255" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
-			</div>
-		</figure>
-	</center>
-	<script type="text/javascript">
-		$('#ca-container').contentcarousel();
-	</script>
-	<div class="container col-sm-offset-2 col-md-8 ">
-		<label for="Associado">Indicado por:</label>
-		<select data-placeholder="Selecione uma opção..." class="form-control" id="Associado" name="Associado">			
-			<!--<option value="">-- Selecione uma Empresa --</option>-->
-			<?php
-			foreach ($select['Associado'] as $key => $row) {
-				(!$query['Associado']) ? $query['Associado'] = $_SESSION['log']['idSis_Empresa'] : FALSE;
-				if ($query['Associado'] == $key) {
-					echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
-				} else {
-					echo '<option value="' . $key . '">' . $row . '</option>';
-				}
-			}
-			?>   
-		</select> 
-		<?php echo form_error('Associado'); ?>
-		<br>
-		
-		<label for="NomeEmpresa">Nome da Empresa:</label>
-		<input type="text" class="form-control" id="NomeEmpresa" maxlength="45" 
-			   name="NomeEmpresa" value="<?php echo $query['NomeEmpresa']; ?>">
-		<?php echo form_error('NomeEmpresa'); ?>
-		<br>
-		<!--
-		<label for="NumUsuarios">Nº de Usuários:*</label>
-		<select data-placeholder="Selecione uma opção..." class="form-control" id="NumUsuarios" name="NumUsuarios">			
-			<option value="">-- Selecione uma NumUsuarios --</option>
-			<?php
-			foreach ($select['NumUsuarios'] as $key => $row) {
-				(!$query['NumUsuarios']) ? $query['NumUsuarios'] = '1' : FALSE;
-				if ($query['NumUsuarios'] == $key) {
-					echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
-				} else {
-					echo '<option value="' . $key . '">' . $row . '</option>';
-				}
-			}
-			?>   
-		</select> 
-		<?php #echo form_error('NumUsuarios'); ?>
-		<br>
-		-->	
-		<label for="NomeAdmin">Nome do Administrador:</label>
-		<input type="text" class="form-control" id="NomeAdmin" maxlength="255"
-			   name="NomeAdmin" value="<?php echo $query['NomeAdmin']; ?>">
-		<?php echo form_error('NomeAdmin'); ?>
-		<br>
+	<div class="container col-md-5 ">
+		<div class="row">	
+			<center>
+				<figure>
+					<div class="boxVideo">
+						<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
+						<iframe width="230" height="230" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
+					</div>
+				</figure>
+			</center>
+			<script type="text/javascript">
+				$('#ca-container').contentcarousel();
+			</script>
 
-		<label for="Celular">Celular do Administrador:</label>
-		<input type="text" class="form-control Celular Celular" id="Celular" maxlength="11"
-			   name="Celular" placeholder="(XX)999999999" value="<?php echo $query['Celular']; ?>">
-		<?php echo form_error('Celular'); ?>
-		<br>
-		
-		<label for="DataNascimento">Data de Nascimento:</label>
-		<input type="text" class="form-control Date" id="inputDate0" maxlength="10"
-			   name="DataNascimento" placeholder="DD/MM/AAAA" value="<?php echo $query['DataNascimento']; ?>">
-		<?php echo form_error('DataNascimento'); ?>
-		<br>
-
-		<label for="Sexo">Sexo:</label>
-		<select data-placeholder="Selecione um TROCA..." class="form-control" id="Sexo" name="Sexo">
-			<option value=""></option>
-			<?php
-			foreach ($select['Sexo'] as $key => $row) {
-				if ($query['Sexo'] == $key) {
-					echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
-				} else {
-					echo '<option value="' . $key . '">' . $row . '</option>';
+			<label for="Associado">Indicado por:</label>
+			<select data-placeholder="Selecione uma opção..." class="form-control" id="Associado" name="Associado">			
+				<!--<option value="">-- Selecione uma Empresa --</option>-->
+				<?php
+				foreach ($select['Associado'] as $key => $row) {
+					(!$query['Associado']) ? $query['Associado'] = $_SESSION['log']['idSis_Empresa'] : FALSE;
+					if ($query['Associado'] == $key) {
+						echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
+					} else {
+						echo '<option value="' . $key . '">' . $row . '</option>';
+					}
 				}
-			}
-			?>
-		</select>
-		<?php echo form_error('Sexo'); ?>
-		<br>		
-		<!--	
-		<label class="text-">E-mail:</label>
-		<input type="text" class="form-control" id="Email" maxlength="100"
-			   name="Email" value="<?php #echo $query['Email']; ?>">
-		<?php #echo form_error('Email'); ?>
-		<br>
-		
-		<label for="UsuarioEmpresa">Usuário Admin:</label>
-		<input type="text" class="form-control" id="UsuarioEmpresa" maxlength="45"
-			   name="UsuarioEmpresa" value="<?php #echo $query['UsuarioEmpresa']; ?>">
-		<?php #echo form_error('UsuarioEmpresa'); ?>
-		<br>
-		-->
-		<label for="CpfAdmin">CPF do Administrador:</label>
-		<input type="text" class="form-control " id="CpfAdmin" maxlength="11"
-			   name="CpfAdmin" placeholder="99999999999" value="<?php echo $query['CpfAdmin']; ?>">
-		<?php echo form_error('CpfAdmin'); ?>
-		<br>
-		
-		<label for="Senha">Senha:</label>
-		<input type="password" class="form-control" id="Senha" maxlength="45"
-			   name="Senha" value="<?php echo $query['Senha']; ?>">
-		<?php echo form_error('Senha'); ?>
-		<br>
+				?>   
+			</select> 
+			<?php echo form_error('Associado'); ?>
+			<br>
+			
+			<label for="NomeEmpresa">Nome da Empresa:</label>
+			<input type="text" class="form-control" id="NomeEmpresa" maxlength="45" 
+				   name="NomeEmpresa" value="<?php echo $query['NomeEmpresa']; ?>">
+			<?php echo form_error('NomeEmpresa'); ?>
+			<br>
+			<!--
+			<label for="NumUsuarios">Nº de Usuários:*</label>
+			<select data-placeholder="Selecione uma opção..." class="form-control" id="NumUsuarios" name="NumUsuarios">			
+				<option value="">-- Selecione uma NumUsuarios --</option>
+				<?php
+				foreach ($select['NumUsuarios'] as $key => $row) {
+					(!$query['NumUsuarios']) ? $query['NumUsuarios'] = '1' : FALSE;
+					if ($query['NumUsuarios'] == $key) {
+						echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
+					} else {
+						echo '<option value="' . $key . '">' . $row . '</option>';
+					}
+				}
+				?>   
+			</select> 
+			<?php #echo form_error('NumUsuarios'); ?>
+			<br>
+			-->	
+			<label for="NomeAdmin">Nome do Administrador:</label>
+			<input type="text" class="form-control" id="NomeAdmin" maxlength="255"
+				   name="NomeAdmin" value="<?php echo $query['NomeAdmin']; ?>">
+			<?php echo form_error('NomeAdmin'); ?>
+			<br>
 
-		<label for="Senha">Confirmar Senha:</label>
-		<input type="password" class="form-control" id="Confirma" maxlength="45"
-			   name="Confirma" value="<?php echo $query['Confirma']; ?>">
-		<?php echo form_error('Confirma'); ?>
-		<br>	
-		<!--
-		<button class="btn btn-lg btn-warning btn-block" type="submit">REGISTRAR</button>
-		<br>
-		-->
-		<button  class="btn btn-lg btn-success btn-block" type="button" data-toggle="modal" data-loading-text="Aguarde..." data-target=".bs-excluir-modal-sm">
-			<span class="glyphicon glyphicon-floppy-saved"></span> Registrar
-		</button>
-		<br>
-		
-		<a class="btn btn-lg btn-danger btn-block" href="<?php echo base_url(); ?>login/index" role="button">
-			<span class="glyphicon glyphicon-floppy-remove"></span>	Cancelar
-		</a>		
-		<!--
-		<a class="btn btn btn-primary btn-block" href="<?php echo base_url(); ?>login/index" role="button">Acesso dos Usuários</a>
-		<a class="btn btn btn-warning btn-block" href="<?php echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>		
-		-->
-		<br><br>
+			<label for="Celular">Celular do Administrador:</label>
+			<input type="text" class="form-control Celular Celular" id="Celular" maxlength="11"
+				   name="Celular" placeholder="(XX)999999999" value="<?php echo $query['Celular']; ?>">
+			<?php echo form_error('Celular'); ?>
+			<br>
+		</div>
+	</div>
+	<div class="col-md-1 "></div>
+	<div class="container col-md-5 ">	
+		<div class="row">			
+			<label for="DataNascimento">Data de Nascimento:</label>
+			<input type="text" class="form-control Date" id="inputDate0" maxlength="10"
+				   name="DataNascimento" placeholder="DD/MM/AAAA" value="<?php echo $query['DataNascimento']; ?>">
+			<?php echo form_error('DataNascimento'); ?>
+			<br>
+
+			<label for="Sexo">Sexo:</label>
+			<select data-placeholder="Selecione um TROCA..." class="form-control" id="Sexo" name="Sexo">
+				<option value=""></option>
+				<?php
+				foreach ($select['Sexo'] as $key => $row) {
+					if ($query['Sexo'] == $key) {
+						echo '<option value="' . $key . '" selected="selected">' . $row . '</option>';
+					} else {
+						echo '<option value="' . $key . '">' . $row . '</option>';
+					}
+				}
+				?>
+			</select>
+			<?php echo form_error('Sexo'); ?>
+			<br>		
+			<!--	
+			<label class="text-">E-mail:</label>
+			<input type="text" class="form-control" id="Email" maxlength="100"
+				   name="Email" value="<?php #echo $query['Email']; ?>">
+			<?php #echo form_error('Email'); ?>
+			<br>
+			
+			<label for="UsuarioEmpresa">Usuário Admin:</label>
+			<input type="text" class="form-control" id="UsuarioEmpresa" maxlength="45"
+				   name="UsuarioEmpresa" value="<?php #echo $query['UsuarioEmpresa']; ?>">
+			<?php #echo form_error('UsuarioEmpresa'); ?>
+			<br>
+			-->
+			<label for="CpfAdmin">CPF do Administrador:</label>
+			<input type="text" class="form-control " id="CpfAdmin" maxlength="11"
+				   name="CpfAdmin" placeholder="99999999999" value="<?php echo $query['CpfAdmin']; ?>">
+			<?php echo form_error('CpfAdmin'); ?>
+			<br>
+			
+			<label for="Senha">Senha:</label>
+			<input type="password" class="form-control" id="Senha" maxlength="45"
+				   name="Senha" value="<?php echo $query['Senha']; ?>">
+			<?php echo form_error('Senha'); ?>
+			<br>
+
+			<label for="Senha">Confirmar Senha:</label>
+			<input type="password" class="form-control" id="Confirma" maxlength="45"
+				   name="Confirma" value="<?php echo $query['Confirma']; ?>">
+			<?php echo form_error('Confirma'); ?>
+			<br>	
+			<!--
+			<button class="btn btn-lg btn-warning btn-block" type="submit">REGISTRAR</button>
+			<br>
+			-->
+			<button  class="btn btn-lg btn-success btn-block" type="button" data-toggle="modal" data-loading-text="Aguarde..." data-target=".bs-excluir-modal-sm">
+				<span class="glyphicon glyphicon-floppy-saved"></span> Registrar
+			</button>
+			<br>
+			
+			<a class="btn btn-lg btn-danger btn-block" href="<?php echo base_url(); ?>login/index" role="button">
+				<span class="glyphicon glyphicon-floppy-remove"></span>	Cancelar
+			</a>		
+			<!--
+			<a class="btn btn btn-primary btn-block" href="<?php echo base_url(); ?>login/index" role="button">Acesso dos Usuários</a>
+			<a class="btn btn btn-warning btn-block" href="<?php echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>		
+			-->
+		</div>	
+
 	</div>
 
 </div>
