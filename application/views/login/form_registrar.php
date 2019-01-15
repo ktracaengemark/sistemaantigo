118989241f8e842987763f3479a09639c2e7a586
ktracaengemark-Itau <marciorodeng@gmail.com>
2019-01-15 18:48:38 -0200

    Vídeos e Patrocinadores nas Telas de Cadastro de Pessoal e Empresa

--- application/views/login/form_registrar.php
+++ application/views/login/form_registrar.php
@@ -1,66 +1,61 @@
 <?php if (isset($msg)) echo $msg; ?>
 
-<div class="col-sm-offset-4 col-md-4 ">	
-	<div class="container col-sm-offset-2 col-md-9 ">
-
-		<?php #echo validation_errors(); ?>
-
-		<?php echo form_open('login/registrar', 'role="form"'); ?>
-		<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header bg-danger">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Ao "Confirmar Registro",<br>
-												Você concorda com a nossa Política de Privacidade!</h4>
+<div class="container col-sm-offset-3 col-md-3 ">
+	<?php echo form_open('login/registrar', 'role="form"'); ?>
+	
+	<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-danger">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Ao "Confirmar Registro",<br>
+											Você concorda com a nossa Política de Privacidade!</h4>
+				</div>
+				
+				<div class="modal-body">
+					<p>Política de Privacidade.....</p>
+				</div>
+				
+				<div class="modal-footer">
+					<div class="form-group col-md-4 text-left">
+						<div class="form-footer">
+							<button  class="btn btn-success btn-block" type="submit">
+								<span class="glyphicon glyphicon-floppy-saved"></span> Confirmar Registro
+							</button>
+						</div>
 					</div>
-					
-					<div class="modal-body">
-						<p>Política de Privacidade.....</p>
-					</div>
-					
-					<div class="modal-footer">
-						<div class="form-group col-md-4 text-left">
-							<div class="form-footer">
-								<button  class="btn btn-success btn-block" type="submit">
-									<span class="glyphicon glyphicon-floppy-saved"></span> Confirmar Registro
+					<div class="form-group col-md-4 text-left">
+							<div class="form-footer ">
+								<button type="button" class="btn btn-primary btn-block" data-dismiss="modal">
+									<span class="glyphicon glyphicon-remove"> Fechar
 								</button>
 							</div>
 						</div>
-						<div class="form-group col-md-4 text-left">
-								<div class="form-footer ">
-									<button type="button" class="btn btn-primary btn-block" data-dismiss="modal">
-										<span class="glyphicon glyphicon-remove"> Fechar
-									</button>
-								</div>
-							</div>
-						<div class="form-group col-md-4 text-right">
-							<div class="form-footer">		
-								<a class="btn btn-danger btn-block" href="<?php echo base_url() ?>login/index" role="button">
-									<span class="glyphicon glyphicon-floppy-remove"></span> Cancelar
-								</a>
-							</div>	
-						</div>
+					<div class="form-group col-md-4 text-right">
+						<div class="form-footer">		
+							<a class="btn btn-danger btn-block" href="<?php echo base_url() ?>login/index" role="button">
+								<span class="glyphicon glyphicon-floppy-remove"></span> Cancelar
+							</a>
+						</div>	
 					</div>
 				</div>
 			</div>
 		</div>
+	</div>
 
-		<!--
-		<p class="text-center">
-			<a href="<?php echo base_url() . '/' . $_SESSION['log']['modulo']; ?>">
-			<a href="<?php echo base_url() ?>">
-				<img src="<?php echo base_url() . 'arquivos/imagens/' . $_SESSION['log']['modulo'] . '.png' ; ?>" width="25%" />
-			</a>
-		</p>
-		-->
-		<p class="text-center">
-			<a href="<?php echo base_url(); ?>login">
-				<img src="<?php echo base_url() . 'arquivos/imagens/' . $modulo . '.png'; ?>" />
-			</a>
-		</p>
-		<h2 class="form-signin-heading text-center">Conta Pessoal</h2>
-
+	<h2 class="form-signin-heading text-center">enkontraki</h2>		 
+	<center>
+		<figure>
+			<div class="boxVideo">
+				<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
+				<iframe width="255" height="255" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</figure>
+	</center>
+	<script type="text/javascript">
+		$('#ca-container').contentcarousel();
+	</script>	
+	<div class="container col-sm-offset-2 col-md-8 ">
 		<label class="sr-only">Empresa</label>
 		<select data-placeholder="Selecione uma opção..." class="form-control" id="idSis_Empresa" name="idSis_Empresa">			
 			
@@ -162,8 +157,37 @@
 		</a>
 		<!--<a class="btn btn btn-warning btn-block" href="<?php #echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>-->
 		<br><br>
-	</form>
-	
+
 	</div>
 	
 </div>
+<div class="container col-md-4 text-center">
+	<center>
+	<h2 class="form-signin-heading text-center">patrocinadores</h2>	
+	</center>	
+	<div class="row">
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/espmmoda" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-43.png'; ?>" alt="..." class="team-img"><h5>Mãos a Moda</h5></a>
+		  </div>
+		</div>
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/npastor" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-44.png'; ?>" alt="..." class="team-img"><h5>NPastor Advog.</h5></a>
+		  </div>
+		</div>				
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/agevet" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-7.png'; ?>" alt="..." class="team-img"><h5>AgeVet</h5></a>
+		  </div>
+		</div>
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/elefantax" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-33.png'; ?>" alt="..." class="team-img"><h5>Elefantax</h5></a>
+		  </div>
+		</div>			
+	</div>
+</div>
+<div class="container col-md-2 text-center"></div>	
--- application/views/loginempresa/form_registrar.php
+++ application/views/loginempresa/form_registrar.php
@@ -1,71 +1,67 @@
 <?php if (isset($msg)) echo $msg; ?>	
 	
-<div class="col-sm-offset-4 col-md-4 ">	
-	<div class="container col-sm-offset-2 col-md-9 ">
-
-		<?php #echo validation_errors(); ?>
-
-		<?php echo form_open('loginempresa/registrar', 'role="form"'); ?>
-		<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
-			<div class="modal-dialog" role="document">
-				<div class="modal-content">
-					<div class="modal-header bg-danger">
-						<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-						<h4 class="modal-title">Ao "Confirmar Registro",<br>
-												Você concorda com a nossa Política de Privacidade!</h4>
+<div class="container col-sm-offset-3 col-md-3 ">
+	<?php echo form_open('loginempresa/registrar', 'role="form"'); ?>
+	
+	<div class="modal fade bs-excluir-modal-sm" tabindex="-1" role="dialog" aria-labelledby="mySmallModalLabel">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header bg-danger">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title">Ao "Confirmar Registro",<br>
+											Você concorda com a nossa Política de Privacidade!</h4>
+				</div>
+				
+				<div class="modal-body">
+					<p>Política de Privacidade.....</p>
+				</div>
+				
+				<div class="modal-footer">
+					<div class="form-group col-md-4 text-left">
+						<div class="form-footer">
+							<button  class="btn btn-success btn-block" type="submit">
+								<span class="glyphicon glyphicon-floppy-saved"></span> Confirmar Registro
+							</button>
+						</div>
 					</div>
-					
-					<div class="modal-body">
-						<p>Política de Privacidade.....</p>
-					</div>
-					
-					<div class="modal-footer">
-						<div class="form-group col-md-4 text-left">
-							<div class="form-footer">
-								<button  class="btn btn-success btn-block" type="submit">
-									<span class="glyphicon glyphicon-floppy-saved"></span> Confirmar Registro
+					<div class="form-group col-md-4 text-left">
+							<div class="form-footer ">
+								<button type="button" class="btn btn-primary btn-block" data-dismiss="modal">
+									<span class="glyphicon glyphicon-remove"> Fechar
 								</button>
 							</div>
 						</div>
-						<div class="form-group col-md-4 text-left">
-								<div class="form-footer ">
-									<button type="button" class="btn btn-primary btn-block" data-dismiss="modal">
-										<span class="glyphicon glyphicon-remove"> Fechar
-									</button>
-								</div>
-							</div>
-						<div class="form-group col-md-4 text-right">
-							<div class="form-footer">		
-								<a class="btn btn-danger btn-block" href="<?php echo base_url() ?>login/index" role="button">
-									<span class="glyphicon glyphicon-floppy-remove"></span> Cancelar
-								</a>
-							</div>	
-						</div>
+					<div class="form-group col-md-4 text-right">
+						<div class="form-footer">		
+							<a class="btn btn-danger btn-block" href="<?php echo base_url() ?>login/index" role="button">
+								<span class="glyphicon glyphicon-floppy-remove"></span> Cancelar
+							</a>
+						</div>	
 					</div>
 				</div>
 			</div>
 		</div>
-		<!--
-		<p class="text-center">
-			<a href="<?php echo base_url() . '/' . $_SESSION['log']['modulo']; ?>">
-			<a href="<?php echo base_url() ?>">
-				<img src="<?php echo base_url() . 'arquivos/imagens/' . $_SESSION['log']['modulo'] . '.png' ; ?>" width="25%" />
-			</a>
-		</p>
-		-->
-		<p class="text-center">
-			<a href="<?php echo base_url(); ?>loginempresa/registrar">
-				<img src="<?php echo base_url() . 'arquivos/imagens/' . $modulo . '.png'; ?>" />
-			</a>
-		</p>
-		<h2 class="form-signin-heading text-center">Nova Empresa</h2>
+	</div>
+	<h2 class="form-signin-heading text-center">enkontraki</h2>		 
+	<center>
+		<figure>
+			<div class="boxVideo">
+				<!--<iframe width="270" height="270" src="https://www.youtube.com/embed/videoseries?list=PLPP9yl-2bfZFWltdqkqZ2WSazBo7dnDx1" frameborder="0" allowfullscreen></iframe>-->
+				<iframe width="255" height="255" src="<?php echo base_url() . 'arquivos/videos/apresentacao.mp4'; ?>" frameborder="0" allowfullscreen></iframe>
+			</div>
+		</figure>
+	</center>
+	<script type="text/javascript">
+		$('#ca-container').contentcarousel();
+	</script>
 
+	<div class="container col-sm-offset-2 col-md-8 ">
 		<label for="NomeEmpresa">Nome da Empresa:</label>
 		<input type="text" class="form-control" id="NomeEmpresa" maxlength="45" 
 			   autofocus name="NomeEmpresa" value="<?php echo $query['NomeEmpresa']; ?>">
 		<?php echo form_error('NomeEmpresa'); ?>
 		<br>
-	<!--
+		<!--
 		<label for="NumUsuarios">Nº de Usuários:*</label>
 		<select data-placeholder="Selecione uma opção..." class="form-control" id="NumUsuarios" name="NumUsuarios">			
 			<option value="">-- Selecione uma NumUsuarios --</option>
@@ -82,7 +78,7 @@
 		</select> 
 		<?php #echo form_error('NumUsuarios'); ?>
 		<br>
-	-->	
+		-->	
 		<label for="NomeAdmin">Nome do Administrador:</label>
 		<input type="text" class="form-control" id="NomeAdmin" maxlength="255"
 			   name="NomeAdmin" value="<?php echo $query['NomeAdmin']; ?>">
@@ -116,7 +112,7 @@
 		</select>
 		<?php echo form_error('Sexo'); ?>
 		<br>	
-	<!--	
+		<!--	
 		<label class="text-">E-mail:</label>
 		<input type="text" class="form-control" id="Email" maxlength="100"
 			   name="Email" value="<?php #echo $query['Email']; ?>">
@@ -128,7 +124,7 @@
 			   name="UsuarioEmpresa" value="<?php #echo $query['UsuarioEmpresa']; ?>">
 		<?php #echo form_error('UsuarioEmpresa'); ?>
 		<br>
-	-->
+		-->
 		<label for="CpfAdmin">CPF do Administrador:</label>
 		<input type="text" class="form-control " id="CpfAdmin" maxlength="11"
 			   name="CpfAdmin" placeholder="99999999999" value="<?php echo $query['CpfAdmin']; ?>">
@@ -163,9 +159,36 @@
 		<a class="btn btn btn-warning btn-block" href="<?php echo base_url(); ?>loginempresa/index" role="button">Acesso dos Administradores</a>
 		-->
 		<br><br>
-		
-	</form>
-
 	</div>
-
-</div>	
+	
+</div>
+<div class="container col-md-4 text-center">
+	<center>
+	<h2 class="form-signin-heading text-center">patrocinadores</h2>	
+	</center>	
+	<div class="row">
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/espmmoda" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-43.png'; ?>" alt="..." class="team-img"><h5>Mãos a Moda</h5></a>
+		  </div>
+		</div>
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/npastor" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-44.png'; ?>" alt="..." class="team-img"><h5>NPastor Advog.</h5></a>
+		  </div>
+		</div>				
+	</div>
+	<div class="row">
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/agevet" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-7.png'; ?>" alt="..." class="team-img"><h5>AgeVet</h5></a>
+		  </div>
+		</div>
+		<div class="col-md-6 col-sm-3 col-xs-6">
+		  <div class="thumbnail"> 
+			<a href="https://www.enkontraki.com/elefantax" target="_blank"><img src="<?php echo base_url() . 'arquivos/imagens/patroc/profile-33.png'; ?>" alt="..." class="team-img"><h5>Elefantax</h5></a>
+		  </div>
+		</div>			
+	</div>
+</div>
+<div class="container col-md-2 text-center"></div>	
