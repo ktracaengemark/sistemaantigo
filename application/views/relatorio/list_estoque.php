930ea6c20ff2de001e83e1f58f05c8ebc9fe33a1
marciorodeng <marciorodeng@gmail.com>
2019-11-28 23:55:33 -0300

    Liberado o Relatório de Devoluções
    A Devolução será usada por empresas que trabalham com aluguel de produtos

--- application/views/relatorio/list_estoque.php
+++ application/views/relatorio/list_estoque.php
@@ -13,11 +13,11 @@
 							<!--<th class="active text-left">Aux1</th>
 							<th class="active text-left">Aux2</th>-->
 							<th class="active text-center">QTD COMPRA</th>
-							<th class="active text-center">QTD VENDIDA</th>						
-							<!--<th class="active text-center">QTD ENTREGUE</th>
-							<th class="active text-center">QTD DEV.COMPRA</th>						
-							<th class="active text-center">QTD DEV.VENDA</th>
-							<th class="active text-center">QTD CONSUMO</th>-->
+							<!--<th class="active text-center">QTD VENDIDA</th>-->						
+							<th class="active text-center">QTD ENTREGUE</th>
+							<!--<th class="active text-center">QTD DEV.COMPRA</th>-->						
+							<th class="active text-center">QTD DEVOLVIDA</th>
+							<!--<th class="active text-center">QTD CONSUMO</th>-->
 							<th class="active text-center">QTD ESTOQUE</th>
 						</tr>
 					</thead>
@@ -51,10 +51,10 @@
 								#echo '<td>' . $row->Prodaux1 . '</td>';
 								#echo '<td>' . $row->Prodaux2 . '</td>';
 								echo '<td>' . $row->QtdCompra . '</td>';
-								echo '<td>' . $row->QtdVendida . '</td>';							
-								#echo '<td>' . $row->Qtd . '</td>';
+								#echo '<td>' . $row->QtdVendida . '</td>';							
+								echo '<td>' . $row->Qtd . '</td>';
 								#echo '<td>' . $row->QtdDevolve2 . '</td>';							
-								#echo '<td>' . $row->QtdDevolve . '</td>';
+								echo '<td>' . $row->QtdDevolve . '</td>';
 								#echo '<td>' . $row->QtdConsumo . '</td>';
 								echo '<td>' . $row->QtdEstoque . '</td>';
 							echo '</tr>';
@@ -70,9 +70,9 @@
 							<th colspan="1" class="active"><?php echo $report->soma->somaqtdcompra ?></th>
 							<!--<th colspan="1" class="active"><?php echo $report->soma->somaqtdvendida ?></th>-->			
 							<th colspan="1" class="active"><?php echo $report->soma->somaqtdvenda ?></th>
-							<!--<th colspan="1" class="active"><?php echo $report->soma->somaqtddevolve2 ?></th>						
+							<!--<th colspan="1" class="active"><?php echo $report->soma->somaqtddevolve2 ?></th>-->						
 							<th colspan="1" class="active"><?php echo $report->soma->somaqtddevolve ?></th>
-							<th colspan="1" class="active"><?php echo $report->soma->somaqtdconsumo ?></th>-->
+							<!--<th colspan="1" class="active"><?php echo $report->soma->somaqtdconsumo ?></th>-->
 							<th colspan="1" class="active"><?php echo $report->soma->somaqtdestoque ?></th>
 						</tr>
 					</tfoot>
