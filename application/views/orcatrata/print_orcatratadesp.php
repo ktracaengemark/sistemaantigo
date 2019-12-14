80bdf4ce7ba093eec7f13c5a13b33c26dff82237
marciorodeng <marciorodeng@gmail.com>
2019-12-14 08:42:25 -0300

    Correção do fiado- não pode aparecer para conta pessoal

--- application/views/orcatrata/print_orcatratadesp.php
+++ application/views/orcatrata/print_orcatratadesp.php
@@ -98,15 +98,15 @@
 							<ul class="dropdown-menu" role="menu">							
 								<li><a href="<?php echo base_url() ?>relatorio/financeiro"><span class="glyphicon glyphicon-usd"></span> Orçamentos</a></li>
 								<li role="separator" class="divider"></li>							
-								<li><a href="<?php echo base_url() ?>relatorio/parcelas"><span class="glyphicon glyphicon-usd"></span> Receber X Pagar</a></li>
-								<li role="separator" class="divider"></li>							
-								<li><a href="<?php echo base_url() ?>relatorio/fiado"><span class="glyphicon glyphicon-usd"></span> Fiado</a></li>									
-								<li role="separator" class="divider"></li>									
-								<li><a href="<?php echo base_url() ?>relatorio/balanco"><span class="glyphicon glyphicon-usd"></span> Balanço</a></li>
+								<li><a href="<?php echo base_url() ?>relatorio/parcelas"><span class="glyphicon glyphicon-usd"></span> Receitas X Despesas</a></li>
 								<?php if ($_SESSION['log']['NivelEmpresa'] >= 4 ) { ?>
+								<li role="separator" class="divider"></li>							
+								<li><a href="<?php echo base_url() ?>relatorio/fiado"><span class="glyphicon glyphicon-usd"></span> Fiado X Faturado</a></li>
 								<li role="separator" class="divider"></li>
 								<li><a href="<?php echo base_url() ?>relatorio/rankingvendas"><span class="glyphicon glyphicon-pencil"></span> Ranking de Vendas</a></li>
 								<?php } ?>
+								<li role="separator" class="divider"></li>							
+								<li><a href="<?php echo base_url() ?>relatorio/balanco"><span class="glyphicon glyphicon-usd"></span> Balanço</a></li>									
 							</ul>
 						</div>							
 						<?php if ($_SESSION['log']['NivelEmpresa'] >= 4 ) { ?>
