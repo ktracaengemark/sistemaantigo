439e40c0346aa1e5205063ba8b718754b3c22645
ktracaengemark-Itau <marciorodeng@gmail.com>
2020-01-20 22:24:41 -0200

    limpeza

--- application/controllers/Funcao.php
+++ application/controllers/Funcao.php
@@ -112,80 +112,6 @@ class Funcao extends CI_Controller {
         $this->load->view('basico/footer');
     }
 
-    public function cadastrar3($tabela = NULL) {
-
-        if ($this->input->get('m') == 1)
-            $data['msg'] = $this->basico->msg('<strong>Informações salvas com sucesso</strong>', 'sucesso', TRUE, TRUE, TRUE);
-        elseif ($this->input->get('m') == 2)
-            $data['msg'] = $this->basico->msg('<strong>Erro no Banco de dados. Entre em contato com o administrador deste sistema.</strong>', 'erro', TRUE, TRUE, TRUE);
-        else
-            $data['msg'] = '';
-
-        $data['query'] = quotes_to_entities($this->input->post(array(
-            'idTab_Funcao',
-            'Funcao',
-			'Abrev',
-                ), TRUE));
-
-        $this->form_validation->set_error_delimiters('<div class="alert alert-danger" role="alert">', '</div>');
-
-        $this->form_validation->set_rules('Funcao', 'Nome do Funcao', 'required|trim');
-		$this->form_validation->set_rules('Abrev', 'Abreviação', 'required|trim');
-		
-        $data['titulo'] = 'Cadastrar Funcao';
-        $data['form_open_path'] = 'funcao/cadastrar3';
-        $data['readonly'] = '';
-        $data['disabled'] = '';
-        $data['panel'] = 'primary';
-        $data['metodo'] = 1;
-        $data['button'] =
-                '
-                <button class="btn btn-sm btn-primary" name="pesquisar" value="0" type="submit">
-                    <span class="glyphicon glyphicon-plus"></span> Cadastrar
-                </button>
-        ';
-
-        $data['sidebar'] = 'col-sm-3 col-md-2';
-        $data['main'] = 'col-sm-7 col-md-8';
-
-        $data['q'] = $this->Funcao_model->lista_funcao3(TRUE);
-        $data['list'] = $this->load->view('funcao/list_funcao3', $data, TRUE);
-
-        #run form validation
-        if ($this->form_validation->run() === FALSE) {
-            $this->load->view('funcao/pesq_funcao3', $data);
-        } else {
-
-            $data['query']['Funcao'] = trim(mb_strtoupper($data['query']['Funcao'], 'ISO-8859-1'));
-			$data['query']['Abrev'] = trim(mb_strtoupper($data['query']['Abrev'], 'ISO-8859-1'));
-			$data['query']['idSis_Empresa'] = $_SESSION['log']['id'];
-			$data['query']['idSis_Usuario'] = $_SESSION['log']['id'];
-            $data['query']['idTab_Modulo'] = $_SESSION['log']['idTab_Modulo'];
-
-            $data['campos'] = array_keys($data['query']);
-            $data['anterior'] = array();
-
-            $data['idTab_Funcao'] = $this->Funcao_model->set_funcao($data['query']);
-
-            if ($data['idTab_Funcao'] === FALSE) {
-                $msg = "<strong>Erro no Banco de dados. Entre em contato com o administrador deste sistema.</strong>";
-
-                $this->basico->erro($msg);
-                $this->load->view('funcao/cadastrar3', $data);
-            } else {
-
-                $data['auditoriaitem'] = $this->basico->set_log($data['anterior'], $data['query'], $data['campos'], $data['idTab_Funcao'], FALSE);
-                $data['auditoria'] = $this->Basico_model->set_auditoria($data['auditoriaitem'], 'Tab_Funcao', 'CREATE', $data['auditoriaitem']);
-                $data['msg'] = '?m=1';
-
-                redirect(base_url() . 'funcao/cadastrar3' . $data['msg']);
-                exit();
-            }
-        }
-
-        $this->load->view('basico/footer');
-    }
-
     public function alterar($id = FALSE) {
 
         if ($this->input->get('m') == 1)
@@ -262,83 +188,6 @@ class Funcao extends CI_Controller {
 
         $this->load->view('basico/footer');
     }
-
-    public function alterar3($id = FALSE) {
-
-        if ($this->input->get('m') == 1)
-            $data['msg'] = $this->basico->msg('<strong>Informações salvas com sucesso</strong>', 'sucesso', TRUE, TRUE, TRUE);
-        elseif ($this->input->get('m') == 2)
-            $data['msg'] = $this->basico->msg('<strong>Erro no Banco de dados. Entre em contato com o administrador deste sistema.</strong>', 'erro', TRUE, TRUE, TRUE);
-        else
-            $data['msg'] = '';
-
-        $data['query'] = $this->input->post(array(
-            'idTab_Funcao',
-            'Funcao',
-			'Abrev',
-                ), TRUE);
-
-        if ($id)
-            $data['query'] = $this->Funcao_model->get_funcao($id);
-
-        $this->form_validation->set_error_delimiters('<div class="alert alert-danger" role="alert">', '</div>');
-
-        $this->form_validation->set_rules('Funcao', 'Nome do Funcao', 'required|trim');
-		$this->form_validation->set_rules('Abrev', 'Abreviação', 'required|trim');
-		
-        $data['titulo'] = 'Editar Funcao';
-        $data['form_open_path'] = 'funcao/alterar3';
-        $data['readonly'] = '';
-        $data['disabled'] = '';
-        $data['panel'] = 'primary';
-        $data['metodo'] = 2;
-        $data['button'] =
-                '
-                <button class="btn btn-sm btn-warning" name="pesquisar" value="0" type="submit">
-                    <span class="glyphicon glyphicon-edit"></span> Salvar Alteração
-                </button>
-        ';
-
-        $data['sidebar'] = 'col-sm-3 col-md-2';
-        $data['main'] = 'col-sm-7 col-md-8';
-
-        $data['q'] = $this->Funcao_model->lista_funcao3(TRUE);
-        $data['list'] = $this->load->view('funcao/list_funcao3', $data, TRUE);
-
-        #run form validation
-        if ($this->form_validation->run() === FALSE) {
-            $this->load->view('funcao/pesq_funcao3', $data);
-        } else {
-
-            $data['query']['Funcao'] = trim(mb_strtoupper($data['query']['Funcao'], 'ISO-8859-1'));
-            $data['query']['Abrev'] = trim(mb_strtoupper($data['query']['Abrev'], 'ISO-8859-1'));
-			$data['query']['idSis_Empresa'] = $_SESSION['log']['id'];
-
-            $data['anterior'] = $this->Funcao_model->get_funcao($data['query']['idTab_Funcao']);
-            $data['campos'] = array_keys($data['query']);
-
-            $data['auditoriaitem'] = $this->basico->set_log($data['anterior'], $data['query'], $data['campos'], $data['query']['idTab_Funcao'], TRUE);
-
-            if ($data['auditoriaitem'] && $this->Funcao_model->update_funcao($data['query'], $data['query']['idTab_Funcao']) === FALSE) {
-                $data['msg'] = '?m=2';
-                redirect(base_url() . 'funcao/alterar3/' . $data['msg']);
-                exit();
-            } else {
-
-                if ($data['auditoriaitem'] === FALSE) {
-                    $data['msg'] = '';
-                } else {
-                    $data['auditoria'] = $this->Basico_model->set_auditoria($data['auditoriaitem'], 'Tab_Funcao', 'UPDATE', $data['auditoriaitem']);
-                    $data['msg'] = '?m=1';
-                }
-
-                redirect(base_url() . 'funcao/cadastrar3/' . $data['msg']);
-                exit();
-            }
-        }
-
-        $this->load->view('basico/footer');
-    }
 	
     public function excluir($id = FALSE) {
 
@@ -354,27 +203,6 @@ class Funcao extends CI_Controller {
                 $data['msg'] = '?m=1';
 
 				redirect(base_url() . 'funcao/cadastrar/' . $data['msg']);
-				exit();
-            //}
-        //}
-
-        $this->load->view('basico/footer');
-    }	
-
-    public function excluir3($id = FALSE) {
-
-        if ($this->input->get('m') == 1)
-            $data['msg'] = $this->basico->msg('<strong>Informações salvas com sucesso</strong>', 'sucesso', TRUE, TRUE, TRUE);
-        elseif ($this->input->get('m') == 2)
-            $data['msg'] = $this->basico->msg('<strong>Erro no Banco de dados. Entre em contato com o administrador deste sistema.</strong>', 'erro', TRUE, TRUE, TRUE);
-        else
-            $data['msg'] = '';
-
-                $this->Funcao_model->delete_funcao($id);
-
-                $data['msg'] = '?m=1';
-
-				redirect(base_url() . 'funcao/cadastrar3/' . $data['msg']);
 				exit();
             //}
         //}
