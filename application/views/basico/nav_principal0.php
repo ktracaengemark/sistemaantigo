3d69788ccd1b2f380b7aab1d0024de4cdd09372a
marciorodeng <marciorodeng@gmail.com>
2020-01-29 07:35:51 -0300

    Limpeza nos navegadores

--- application/views/basico/nav_principal0.php
+++ application/views/basico/nav_principal0.php
@@ -1,46 +1,35 @@
 <nav class="navbar navbar-inverse navbar-fixed-top " role="banner">
 	<div class="container-fluid">
-		<div class="row">
-
-			<div class="col-sm-offset-2 col-md-9 ">
-				<div class="navbar-header ">
-					<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar1">
-						<span class="sr-only">MENU</span> <span class="icon-bar"></span> <span class="icon-bar"></span> <span class="icon-bar"></span>
-					</button>
-					<a class="navbar-brand" href="<?php echo base_url() ?>login/index2"> 
-						 Acesse a Sua Conta
-					</a>
-				</div>
-				<div class="collapse navbar-collapse" id="myNavbar1">
-
-					<ul class="nav navbar-nav navbar-center">
-						<!--
-						<li>
-							<?php #echo form_open(base_url() . 'empresacli/pesquisar', 'class="navbar-form navbar-left"'); ?>
-							<div class="input-group">
-								<span class="input-group-btn">
-									<button class="btn btn-info" type="submit">
-										<span class="glyphicon glyphicon-search"></span>
-									</button>
-								</span>
-								<input type="text" placeholder="Pesquisar Empresa" class="form-control" name="Pesquisa" value="">
-							</div>
-							</form>
-						</li>
-											
-						<li class="btn-toolbar btn-sm navbar-form navbar-right" role="toolbar" aria-label="...">
-							<div class="btn-group " role="group" aria-label="...">
-								<a href="<?php #echo base_url(); ?>login/index">
-									<button type="button" class="btn btn-sm btn-danger ">
-										<span class="glyphicon glyphicon-log-out"></span> Acessar Contas
-									</button>
-								</a>
-							</div>
-						</li>
-						-->	
-					</ul>
-				</div>			
-			</div>
+		<div class="navbar-header ">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar1">
+				<span class="sr-only">MENU</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a type="button" class="navbar-toggle btn btn-lg btn-primary  " href="<?php echo base_url() ?>login/index2"> 
+				<span class="glyphicon glyphicon-remove"></span> Fechar 
+			</a>			
+		</div>
+		<div class="collapse navbar-collapse" id="myNavbar1">		
+			<ul class="nav navbar-nav navbar-center">
+				<li class="btn-toolbar btn-lg navbar-form" role="toolbar" aria-label="...">
+					<div class="btn-group " role="group" aria-label="...">
+						<a href="<?php echo base_url(); ?>pesquisar/empresas" role="button">
+							<button type="button" class="btn btn-lg btn-warning ">
+								<span class="glyphicon glyphicon-search"></span> Pesquisar
+							</button>
+						</a>						
+					</div>					
+				</li>				
+				<li class="btn-toolbar btn-lg navbar-form" role="toolbar" aria-label="...">
+					<div class="btn-group " role="group" aria-label="...">
+						<a type="button" class="btn btn-lg btn-default " href="<?php echo base_url() ?>login/index2"> 
+							<span class="glyphicon glyphicon-remove"></span> Fechar 
+						</a>						
+					</div>					
+				</li>				
+			</ul>			
 		</div>
 	</div>
 </nav>
