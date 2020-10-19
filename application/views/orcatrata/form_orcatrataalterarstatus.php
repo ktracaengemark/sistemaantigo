62a89ed8fc92658927ae7caff1ba6d844187d2cf
marciorodeng <marciorodeng@gmail.com>
2020-10-19 07:17:18 -0300

    Ajuste nos formulários

--- application/views/orcatrata/form_orcatrataalterarstatus.php
+++ application/views/orcatrata/form_orcatrataalterarstatus.php
@@ -20,7 +20,7 @@
 											<div class="row">
 												<div class="col-md-4">
 													<label for="TipoFinanceiro">Tipo de Receita</label>
-													<select data-placeholder="Selecione uma opção..." class="form-control" <?php echo $readonly; ?>
+													<select data-placeholder="Selecione uma opção..." class="form-control Chosen" <?php echo $readonly; ?>
 															id="TipoFinanceiro" name="TipoFinanceiro">
 														<option value="">-- Selecione uma opção --</option>
 														<?php
