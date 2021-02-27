37468c629879ce922594c3717733dd130740ad7f
marciorodeng <marciorodeng@gmail.com>
2021-02-26 22:13:51 -0300

    Ajuste na teja do Site

--- application/views/relatorio/tela_site.php
+++ application/views/relatorio/tela_site.php
@@ -5,17 +5,20 @@
 	<div class="panel-body">
 		<div class="col-md-12">
 			<h2 class="ser-title">Icone!</h2>
-		<?php if (isset($list4)) echo $list4; ?>
+			<?php if (isset($list4)) echo $list4; ?>
+			<h3>Dimensoes de 300 x 300</h3>
 		</div>			
 		<hr class="botm-line">
 		<div class="col-md-12">
 			<h2 class="ser-title">Logo Navegador!</h2>
-		<?php if (isset($list3)) echo $list3; ?>
+			<?php if (isset($list3)) echo $list3; ?>
+			<h3>Dimensoes de 150 x 66</h3>
 		</div>			
 		<hr class="botm-line">
 		<div class="col-md-12">
 			<h2 class="ser-title">Slides!</h2>
-		<?php if (isset($list2)) echo $list2; ?>
+			<?php if (isset($list2)) echo $list2; ?>
+			<h3>Dimensoes de 1902 x 448</h3>
 		</div>			
 		<!--
 		<hr class="botm-line">
