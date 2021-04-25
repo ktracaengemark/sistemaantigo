75eff22e2988eada3b2337f8bdf3a89eb98062f2
marciorodeng <marciorodeng@gmail.com>
2021-04-25 15:37:04 -0300

    Lista de agendamentos apenas para empresa != 5

--- application/views/relatorio/tela_admin.php
+++ application/views/relatorio/tela_admin.php
@@ -21,13 +21,15 @@
 							<div class="row">
 								<div class="col-md-12">
 									<label for=""><h4><b>Agenda</b></h4></label>
-									<div class="form-group col-md-12 text-left">
-										<div class="row">
-											<a  type="button" class="btn btn-md btn-default btn-block text-left" href="<?php echo base_url() ?>relatorio/list_agendamentos" role="button"> 
-												<span class="glyphicon glyphicon-pencil"></span> Relatório de Agendamentos
-											</a>											
-										</div>	
-									</div>
+									<?php if($_SESSION['log']['idSis_Empresa'] != "5") {?>
+										<div class="form-group col-md-12 text-left">
+											<div class="row">
+												<a  type="button" class="btn btn-md btn-default btn-block text-left" href="<?php echo base_url() ?>relatorio/list_agendamentos" role="button"> 
+													<span class="glyphicon glyphicon-pencil"></span> Relatório de Agendamentos
+												</a>											
+											</div>	
+										</div>
+									<?php }?>	
 								</div>
 								<div class="col-md-12">
 									<label for=""><h4><b>Tarefas</b></h4></label>
