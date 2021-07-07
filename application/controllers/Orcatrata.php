ff7e7d59a278ccf325da1a719a2d3a5f1269014c
marciorodeng <marciorodeng@gmail.com>
2021-07-07 18:12:38 -0300

    Baixa de parcelas com 12 - Funcionando

--- application/controllers/Orcatrata.php
+++ application/controllers/Orcatrata.php
@@ -15813,32 +15813,34 @@ class Orcatrata extends CI_Controller {
 		$data['count']['PRCount'] = $j - 1;
 		
 		//$this->load->library('pagination');
-		$config['per_page'] = 10;
-		$config["uri_segment"] = 4;
-		$config['reuse_query_string'] = TRUE;
-		$config['num_links'] = 2;
-		$config['use_page_numbers'] = TRUE;
-		$config['full_tag_open'] = "<ul class='pagination'>";
-		$config['full_tag_close'] = "</ul>";
-		$config['num_tag_open'] = '<li>';
-		$config['num_tag_close'] = '</li>';
-		$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
-		$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
-		$config['next_tag_open'] = "<li>";
-		$config['next_tagl_close'] = "</li>";
-		$config['prev_tag_open'] = "<li>";
-		$config['prev_tagl_close'] = "</li>";
-		$config['first_tag_open'] = "<li>";
-		$config['first_tagl_close'] = "</li>";
-		$config['last_tag_open'] = "<li>";
-		$config['last_tagl_close'] = "</li>";
+
 		$data['Pesquisa'] = '';
 
         if ($id) {
 
 			$config['base_url'] = base_url() . 'Orcatrata/alterarparcelarec/' . $id . '/';
 			$config['total_rows'] = $this->Orcatrata_model->get_alterarparcela($id, TRUE);
-		   
+			
+			$config['per_page'] = 12;
+			$config["uri_segment"] = 4;
+			$config['reuse_query_string'] = TRUE;
+			$config['num_links'] = 2;
+			$config['use_page_numbers'] = TRUE;
+			$config['full_tag_open'] = "<ul class='pagination'>";
+			$config['full_tag_close'] = "</ul>";
+			$config['num_tag_open'] = '<li>';
+			$config['num_tag_close'] = '</li>';
+			$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+			$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+			$config['next_tag_open'] = "<li>";
+			$config['next_tagl_close'] = "</li>";
+			$config['prev_tag_open'] = "<li>";
+			$config['prev_tagl_close'] = "</li>";
+			$config['first_tag_open'] = "<li>";
+			$config['first_tagl_close'] = "</li>";
+			$config['last_tag_open'] = "<li>";
+			$config['last_tagl_close'] = "</li>";		   
+			
 			if($config['total_rows'] >= 1){
 				$_SESSION['Total_Rows'] = $data['total_rows'] = $config['total_rows'];
 			}else{
@@ -16260,32 +16262,34 @@ class Orcatrata extends CI_Controller {
 		$data['count']['PRCount'] = $j - 1;
 		
 		//$this->load->library('pagination');
-		$config['per_page'] = 10;
-		$config["uri_segment"] = 4;
-		$config['reuse_query_string'] = TRUE;
-		$config['num_links'] = 2;
-		$config['use_page_numbers'] = TRUE;
-		$config['full_tag_open'] = "<ul class='pagination'>";
-		$config['full_tag_close'] = "</ul>";
-		$config['num_tag_open'] = '<li>';
-		$config['num_tag_close'] = '</li>';
-		$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
-		$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
-		$config['next_tag_open'] = "<li>";
-		$config['next_tagl_close'] = "</li>";
-		$config['prev_tag_open'] = "<li>";
-		$config['prev_tagl_close'] = "</li>";
-		$config['first_tag_open'] = "<li>";
-		$config['first_tagl_close'] = "</li>";
-		$config['last_tag_open'] = "<li>";
-		$config['last_tagl_close'] = "</li>";
+
 		$data['Pesquisa'] = '';		
 
         if ($id) {
 
 			$config['base_url'] = base_url() . 'Orcatrata/alterarparceladesp/' . $id . '/';
 			$config['total_rows'] = $this->Orcatrata_model->get_alterarparcela($id, TRUE);
-		   
+			
+			$config['per_page'] = 12;
+			$config["uri_segment"] = 4;
+			$config['reuse_query_string'] = TRUE;
+			$config['num_links'] = 2;
+			$config['use_page_numbers'] = TRUE;
+			$config['full_tag_open'] = "<ul class='pagination'>";
+			$config['full_tag_close'] = "</ul>";
+			$config['num_tag_open'] = '<li>';
+			$config['num_tag_close'] = '</li>';
+			$config['cur_tag_open'] = "<li class='disabled'><li class='active'><a href='#'>";
+			$config['cur_tag_close'] = "<span class='sr-only'></span></a></li>";
+			$config['next_tag_open'] = "<li>";
+			$config['next_tagl_close'] = "</li>";
+			$config['prev_tag_open'] = "<li>";
+			$config['prev_tagl_close'] = "</li>";
+			$config['first_tag_open'] = "<li>";
+			$config['first_tagl_close'] = "</li>";
+			$config['last_tag_open'] = "<li>";
+			$config['last_tagl_close'] = "</li>";		   
+			
 			if($config['total_rows'] >= 1){
 				$_SESSION['Total_Rows'] = $data['total_rows'] = $config['total_rows'];
 			}else{
